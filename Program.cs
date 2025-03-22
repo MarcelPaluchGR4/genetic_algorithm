d79d2e0185b2dbbb50263efa38a48b7469a4534b
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-03-22 12:35:07 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GeneticAlgorithm
+{
+  static class GeneticAlgorithm
+  {
+    static void CreatePopulation()
+    {
+      //TODO 
+    }
+
+    static void RatePopulationMembers()
+    {
+      //TODO 
+    }
+
+    static void Main(string[] args)
+    {
+      CreatePopulation();
+      RatePopulationMembers();
+
+    }
+  }
+}
